938871ae79c209e0f5496212b0c241e15b292caa
Kobchok Chuawong <kobchok.chuawong@gmail.com>
2026-04-07 10:47:44 +0700

    connection for production

--- data/postgres.py
+++ data/postgres.py
@@ -3,17 +3,26 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
+# TEST
+# def get_connection() -> psycopg2.extensions.connection | bool:
+#     try:
+#         return psycopg2.connect(
+#             dbname=os.getenv("DATABASE"),
+#             user=os.getenv("DB_USER"),
+#             password=os.getenv("DB_PASSWORD"),
+#             host=os.getenv("DB_HOST"),
+#             port=os.getenv("DB_PORT")
+#         )
+#     except Exception as e:
+#         print(f"Error connecting to PostgreSQL: {e}")
+#         return False
+    
+# PRODUCTION
 def get_connection() -> psycopg2.extensions.connection | bool:
     try:
         return psycopg2.connect(
-            dbname=os.getenv("DATABASE"),
-            user=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASSWORD"),
-            host=os.getenv("DB_HOST"),
-            port=os.getenv("DB_PORT")
+            os.getenv("PSQL_INTERNAL_URL")
         )
     except Exception as e:
         print(f"Error connecting to PostgreSQL: {e}")
         return False
-    
